3ec1637a76db17bcac7c23c850e3cd08947c0c6a
Travis McMahon <azgoalie@gmail.com>
2022-08-03 23:43:39 -0700

    Make steamid.io link open new tab

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,29 +18,26 @@ const Home: NextPage = () => {
     }
   };
 
-  const removeSteamid = (steamid: string) =>
-    setSteamids(steamids.filter((id) => id !== steamid));
+  const removeSteamid = (steamid: string) => setSteamids(steamids.filter((id) => id !== steamid));
 
   const handleFindGames = () => {
     getApps({ variables: { steamids } });
   };
 
   return (
-    <div className="flex h-screen flex-col justify-between gap-4">
+    <div className="flex flex-col justify-between h-screen gap-4">
       <Head>
         <title>Steam Distillery</title>
         <meta name="description" content="Searching Steam, distiled" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="mx-auto mt-12 w-full max-w-screen-2xl p-4">
-        <h1 className="mb-4 text-center text-3xl font-medium text-gray-900 sm:text-4xl">
-          Steam Distillery
-        </h1>
+      <main className="w-full p-4 mx-auto mt-12 max-w-screen-2xl">
+        <h1 className="mb-4 text-3xl font-medium text-center text-gray-900 sm:text-4xl">Steam Distillery</h1>
 
-        <p className="mb-8 text-center text-base leading-relaxed">
+        <p className="mb-8 text-base leading-relaxed text-center">
           Find multiplayer games that you share between friends! Enter a{' '}
-          <a href="https://steamid.io/" className="text-indigo-500 underline">
+          <a href="https://steamid.io/" className="text-indigo-500 underline" target="blank">
             SteamID
           </a>{' '}
           to begin.
@@ -58,7 +55,7 @@ const Home: NextPage = () => {
               type="button"
               onClick={handleFindGames}
               disabled={steamids?.length === 0}
-              className="w-full rounded border-0 bg-indigo-500 py-2 px-6 text-center text-lg text-white hover:bg-indigo-600 focus:outline-none disabled:bg-indigo-300"
+              className="w-full px-6 py-2 text-lg text-center text-white bg-indigo-500 border-0 rounded hover:bg-indigo-600 focus:outline-none disabled:bg-indigo-300"
             >
               Find Games!
             </button>
